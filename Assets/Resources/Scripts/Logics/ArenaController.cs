efeff8d45e3c0a4f4c9dc8b9e4dd624b0793c3ac
Mark <mark.tsemma@gmail.com>
2022-01-21 23:39:54 +0200

    version updated

--- Assets/Resources/Scripts/Logics/ArenaController.cs
+++ Assets/Resources/Scripts/Logics/ArenaController.cs
@@ -193,6 +193,5 @@ public class ArenaController : MonoBehaviourPunCallbacks
     public void LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
-        Launcher.instance.OnFightLoss();
     }
 }
