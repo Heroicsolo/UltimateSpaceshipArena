4734c7e915534c4f2f0beac1e06a2a6c96a5344b
Mark <mark.tsemma@gmail.com>
2022-02-15 23:10:24 +0200

    client version updated

--- Assets/Scripts/Logics/BalanceProvider.cs
+++ Assets/Scripts/Logics/BalanceProvider.cs
@@ -39,7 +39,7 @@ public static class BalanceProvider
     public static BalanceInfo Balance;
     public static bool IsLoaded = false;
     public static Action OnValueChanged;
-    const int clientVersion = 3;
+    const int clientVersion = 4;
 
     public static void Init()
     {
